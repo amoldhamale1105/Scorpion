f9eb3c35c06973d682ca726019df1b579b54cd97
Amol Dhamale <amoldhamale1105@gmail.com>
2021-04-30 17:09:04 +0530

    implementing my custom heap

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include "idt/idt.h"
 #include "io/io.h"
+#include "memory/heap/kheap.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -76,6 +77,20 @@ void kernel_main()
     terminal_initialize();
     print("Hello World!\ntest");
 
+    //Initialise the heap
+    kheap_init();
+
     //Initialize the interrupt descriptor table
     idt_init();
+
+    void* ptr = kmalloc(50);
+    void* ptr2 = kmalloc(5000);
+    void* ptr3 = kmalloc(5600);
+    kfree(ptr);
+    void* ptr4 = kmalloc(50);
+
+    if (ptr || ptr2 || ptr3 || ptr4)
+    {
+        //temporarily added to evade compiler warnings of unused pointers
+    }
 }
