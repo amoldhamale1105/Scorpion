f8236f8824337f8648d2ea51d97960a318fc6b06
Amol Dhamale <amoldhamale1105@gmail.com>
2021-05-18 10:54:30 +0530

    implemented VFS fopen, added few macros and string operation functions

--- src/string/string.c
+++ src/string/string.c
@@ -1,5 +1,17 @@
 #include "string.h"
 
+char tolower(char s1)
+{
+    //check if its a capital letter starting with ASCII 65 = 'A'
+    if (s1 >= 65 && s1 <= 90)
+    {
+        //add 32 to convert it to lower case ex. 65+32 = 97 = 'a' in ASCII table
+        s1+= 32;
+    }
+
+    return s1;
+}
+
 int strlen(const char* ptr)
 {
     int len = 0;
@@ -20,6 +32,61 @@ int strnlen(const char* ptr, int max)
     }
 
     return len;
+}
+
+int istrncmp(const char* s1, const char* s2, int n)
+{
+    unsigned char u1, u2;
+    while (n-- > 0)
+    {
+        u1 = (unsigned char)*s1++;
+        u2 = (unsigned char)*s2++;
+        if (u1 != u2 && tolower(u1) != tolower(u2))
+        {
+            return u1 -u2;
+        }
+        if (u1 == '\0')
+        {
+            return 0;
+        }
+    }
+    return 0;
+}
+
+int strncmp(const char* str1, const char* str2, int n)
+{
+    unsigned char u1, u2;
+
+    while (n-- > 0)
+    {
+        u1 = (unsigned char)*str1++;
+        u2 = (unsigned char)*str2++;
+
+        if (u1 != u2)
+        {
+            return u1 - u2;
+        }
+        if (u1 == '\0')
+        {
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+int strnlen_terminator(const char* str, int max, char terminator)
+{
+    int i = 0;
+    for (i = 0; i < max; i++)
+    {
+        if (str[i] == '\0' || str[i] == terminator)
+        {
+            break;
+        }
+    }
+
+    return i;
 }
 
 char* strcpy(char* dest, const char* src)
