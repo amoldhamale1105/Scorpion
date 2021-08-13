e68d337b35868997ffa6b78939e7f0ab6a5a8342
Amol Dhamale <amoldhamale1105@gmail.com>
2021-08-13 12:21:27 +0530

    implemented Scorpion's very own shell interface!

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -6,13 +6,6 @@ int main(int argc, char** argv)
 {
     printf("Hello, I am %i years old\n", 116);
 
-    void* ptr = malloc(512);
-    free(ptr);
-    
-    char buf[1024];
-    scorpion_terminal_readline(buf, sizeof(buf), true);
-    print(buf);
-    
     while(1) 
     {
     }
---
+++ programs/shell/src/shell.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "scorpion.h"
+
+int main(int argc, char** argv)
+{   
+    print("Scorpion v1.0.0\n");
+    while(1)
+    {
+        print("\n> ");
+        char buf[1024];
+        scorpion_terminal_readline(buf, sizeof(buf), true);
+    }
+    return 0;
+}
