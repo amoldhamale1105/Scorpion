d39b40d39301eb41150c14be8a965661928b2360
Amol Dhamale <amoldhamale1105@gmail.com>
2021-09-01 10:54:13 +0530

    adding command argument parsing logic

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -5,14 +5,11 @@
 
 int main(int argc, char** argv)
 {
-    char* ptr = malloc(20);
-    strcpy(ptr, "hello world");
-    print(ptr);
-    free(ptr);
-
-    ptr[0] = 'B';
-    print("abc\n");
-
+    char str[] = "hello world";
+    struct command_argument* root_command = scorpion_parse_command(str, sizeof(str));
+    printf("%s\n", root_command->argument);
+    printf("%s\n", root_command->next->argument);
+    
     while(1) 
     {
     }
--- programs/stdlib/src/scorpion.h
+++ programs/stdlib/src/scorpion.h
@@ -4,6 +4,12 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+struct command_argument
+{
+    char argument[512];
+    struct command_argument* next;
+};
+
 void print(const char* message);
 int scorpion_getkey();
 void* scorpion_malloc(size_t size);
@@ -12,5 +18,6 @@ void scorpion_putchar(char c);
 int scorpion_getkeyblock();
 void scorpion_terminal_readline(char* out, int max, bool output_while_typing);
 void scorpion_process_load_start(const char* filename);
+struct command_argument* scorpion_parse_command(const char* command, int max);
 
 #endif
