3aee4841e71099cd27e9c2dcd49b5c53031d0c8a
Amol Dhamale <amoldhamale1105@gmail.com>
2021-09-07 10:52:48 +0530

    bugfixes and resolving memory leaks

--- src/fs/fat/fat16.c
+++ src/fs/fat/fat16.c
@@ -206,6 +206,7 @@ out:
 int fat16_get_root_directory(struct disk* disk, struct fat_private* fat_private, struct fat_directory* directory)
 {
     int res = 0;
+    struct fat_directory_item* dir = 0;
     struct fat_header* primary_header = &fat_private->header.primary_header;
     int root_dir_sector_pos = (primary_header->fat_copies * primary_header->sectors_per_fat) + primary_header->reserved_sectors;
     int root_dir_entries = fat_private->header.primary_header.root_dir_entries;
@@ -219,24 +220,24 @@ int fat16_get_root_directory(struct disk* disk, struct fat_private* fat_private,
 
     int total_items = fat16_get_total_items_for_directory(disk, root_dir_sector_pos);
 
-    struct fat_directory_item* dir = kzalloc(root_dir_size);
+    dir = kzalloc(root_dir_size);
     if (!dir)
     {
         res = -ENOMEM;
-        goto out;
+        goto err_out;
     }
 
     struct disk_stream* stream = fat_private->directory_stream;
     if (diskstreamer_seek(stream, fat16_sector_to_absolute(disk, root_dir_sector_pos)) != SCORPION_ALL_OK)
     {
         res = -EIO;
-        goto out;
+        goto err_out;
     }
 
     if (diskstreamer_read(stream, dir, root_dir_size) != SCORPION_ALL_OK)
     {
         res = -EIO;
-        goto out;
+        goto err_out;
     }
 
     directory->item = dir;
@@ -245,6 +246,13 @@ int fat16_get_root_directory(struct disk* disk, struct fat_private* fat_private,
     directory->ending_sector_pos = root_dir_sector_pos + (root_dir_size / disk->sector_size);
 
 out:
+    return res;
+
+err_out:
+    if (dir)
+    {
+        kfree(dir);
+    }
     return res;
 }
 
@@ -570,6 +578,7 @@ struct fat_item* fat16_new_fat_item_for_directory_item(struct disk* disk, struct
     {
         f_item->directory = fat16_load_fat_directory(disk, item);
         f_item->type = FAT_ITEM_TYPE_DIRECTORY;
+        return f_item;
     }
 
     f_item->type = FAT_ITEM_TYPE_FILE;
@@ -627,27 +636,38 @@ out:
 
 void* fat16_open(struct disk* disk, struct path_part* path, FILE_MODE mode)
 {
+    struct fat_file_descriptor* descriptor = 0;
+    int err_code = 0;
     if (mode != FILE_MODE_READ)
     {
-        return ERROR(-ERDONLY);
+        err_code = -ERDONLY;
+        goto err_out;
     }
 
-    struct fat_file_descriptor* descriptor = 0;
     descriptor = kzalloc(sizeof(struct fat_file_descriptor));
     if (!descriptor)
     {
-        return ERROR(-ENOMEM);
+        err_code = -ENOMEM;
+        goto err_out;
     }
 
     descriptor->item = fat16_get_directory_entry(disk, path);
     if (!descriptor->item)
     {
-        return ERROR(-EIO);
+        err_code = -EIO;
+        goto err_out;
     }
 
     descriptor->pos = 0;
 
     return descriptor;
+
+err_out:
+    if (descriptor)
+    {
+        kfree(descriptor);
+    }
+    return ERROR(err_code);
 }
 
 static void fat16_free_file_descriptor(struct fat_file_descriptor* desc)
